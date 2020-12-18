920d873816c23368c45f390c52cbd415becbd826
Tech-Recruiter-Justin <justin7623@gmail.com>
2020-12-18 17:42:41 +0800

    S25262

---
+++ DOM_Events_CODE/Form_Events/app.js
@@ -0,0 +1,20 @@
+const tweetForm = document.querySelector('#tweetForm')
+const showTweet = document.querySelector('#showTweet')
+tweetForm.addEventListener('submit', function (e){
+    e.preventDefault();
+    const usernameInput = tweetForm.elements.username;
+    const tweetInput = tweetForm.elements.tweet;
+    addTweet(username.value, tweet.value);
+    usernameInput.value = '';
+    tweetInput.value = '';
+});
+
+const addTweet = (username, tweet) => {
+
+    const newTweet = document.createElement('li');
+    const bTag = document.createElement('b');
+    bTag.append(username);
+    newTweet.append(bTag);
+    newTweet.append(` - ${tweet}`);
+    showTweet.append(newTweet);
+}
