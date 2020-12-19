43b5b2f4b3f5b776fbdf208e0c776972d89f6312
Tech-Recruiter-Justin <justin7623@gmail.com>
2020-12-19 09:03:43 +0800

    Update app.js

--- DOM_Events_CODE/PingPong/app.js
+++ DOM_Events_CODE/PingPong/app.js
@@ -2,6 +2,7 @@ let p1 = 0;
 let p2 = 0;
 let game1 = 0;
 let game2 = 0;
+let status = "normal";
 const p1Score = document.querySelector("#p1Score");
 const p2Score = document.querySelector("#p2Score");
 const p1Plus = document.querySelector("#p1Plus");
@@ -9,21 +10,14 @@ const p2Plus = document.querySelector("#p2Plus");
 const p1Game = document.querySelector("#p1Game");
 const p2Game = document.querySelector("#p2Game");
 
-
-
 p1Plus.addEventListener('click', function(e){
     p1++;
     p1Score.innerText = p1;
-
     if (p1 === 11){
         game1 = game1 + 1;
         p1Game.innerText = game1;
-        p1 = 0;
-        p1Score.innerText = p1;
-        p2 = 0;
-        p2Score.innerText = p2;
+        gameDone();
     };
-
     if (p1 === p2 === 10){
         duce();
     }
@@ -36,17 +30,15 @@ p2Plus.addEventListener('click', function(e){
     if (p2 === 11){
         game2 = game2 + 1;
         p2Game.innerText = game2;
-        p1 = 0;
-        p1Score.innerText = p1;
-        p2 = 0;
-        p2Score.innerText = p2;
+        gameDone();
     };
     console.log(game2);
 });
 
-duse = (p1, p2) => {
-    
-}
-
-
+gameDone = () => {
+    p1 = 0;
+    p1Score.innerText = p1;
+    p2 = 0;
+    p2Score.innerText = p2;
+};
 
