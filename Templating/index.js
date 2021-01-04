727815e2a337a4766f877a1fe3074cc140b455cc
Tech-Recruiter-Justin <justin7623@gmail.com>
2021-01-04 23:06:18 +0800

    S34345

---
+++ Templating/index.js
@@ -0,0 +1,24 @@
+const express = require('express');
+const app = express();
+const path = require('path');
+
+app.set('view engine', 'ejs');
+app.set('views', path.join(__dirname, '/views'));
+
+app.get('/', (req, res) => {
+    res.render('home');
+})
+
+app.get('/random', (req, res) => {
+    const random = Math.floor(Math.random() * 10) + 1;
+    res.render('random', { random });
+})
+
+app.get('/cats', (req, res) => {
+    const cats = ['A', 'B', 'C', 'D'];
+    res.render('cats', {cats});
+})
+
+app.listen(3000, () => {
+    console.log("Listening on port 3000");
+})
