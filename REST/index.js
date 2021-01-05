cf7f5d1933c92af269068e50bba2f08dead8b489
Tech-Recruiter-Justin <justin7623@gmail.com>
2021-01-05 21:37:23 +0800

    S35356

---
+++ REST/index.js
@@ -0,0 +1,40 @@
+const express = require('express');
+const app = express();
+const path = require('path');
+
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+app.set('views', path.join(__dirname, 'views'));
+app.set('view engine', 'ejs');
+
+const comments = [
+    {
+        username: 'Justin',
+        comment: 'That is yummuy!' 
+    },
+    {
+        username: 'Stephanie',
+        comment: 'Do not eat too much!'
+    },
+    {
+        username: 'Justin',
+        comment: 'It is okay~'
+    }
+]
+
+app.get('/comments', (req, res) => {
+    res.render('comments/index', { comments });
+})
+
+app.get('/tacos', (req, res) =>{
+    res.send("GET /tacos response");
+})
+
+app.post('/tacos', (req, res) =>{
+    const { meat, qty } = req.body;
+    res.send(`OK, here are your ${qty} ${meat} tacos`);
+})
+
+app.listen(3000, () => {
+    console.log("ON PORT 3000");
+})
